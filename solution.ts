a6dd72db066496061747c08eede1afe15329dd08
Md-shahien-khan <shawonkn58@gmail.com>
2025-11-17 17:43:02 +0000

    problem solving 2

--- solution.ts
+++ solution.ts
@@ -3,3 +3,13 @@ const formatValue = (value: string | number | boolean): string | number | boolea
   if (typeof value === "number") return value * 10;
   return !value; 
 };
+
+
+
+const getLength = (value: string | any[]): number => {
+  if (typeof value === "string") return value.length;
+  if (Array.isArray(value)) return value.length;
+  throw new Error("Invalid type");
+};
+
+
